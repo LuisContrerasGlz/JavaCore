09e46fc709a932d7b8573018d80d757d39aac2da
Luis Francisco Contreras Gonzalez <51728784+LuisContrerasGlz@users.noreply.github.com>
2021-09-11 17:30:58 -0500

    Update operations.java

--- src/javaAssignment1/operations.java
+++ src/javaAssignment1/operations.java
@@ -1,4 +1,10 @@
 package javaAssignment1;
+/* Write a Java program to print the result of the following operations. 
+Test Data:
+a. -5 + 8 * 6
+b. (55+9) % 9 
+c. 20 + -3*5 / 8 
+d. 5 + 15 / 3 * 2 - 8 % 3 */
 
 public class operations {
 
