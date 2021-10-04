d5e8beca2e3486549e914b10ca9e1a6f35dd46e8
Luis Francisco Contreras Gonzalez <51728784+LuisContrerasGlz@users.noreply.github.com>
2021-10-03 22:49:03 -0500

    Update CowDemo.java

--- src/abstractionDemo/CowDemo.java
+++ src/abstractionDemo/CowDemo.java
@@ -3,7 +3,7 @@ package abstractionDemo;
 public class CowDemo extends AnimalDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		teststatic();
 		
 		CowDemo oo=new CowDemo();
@@ -14,7 +14,7 @@ public class CowDemo extends AnimalDemo {
 
 	@Override
 	void eat() {
-		// TODO Auto-generated method stub
+		
 		System.out.println("Cow eats Veg");
 		
 	}
