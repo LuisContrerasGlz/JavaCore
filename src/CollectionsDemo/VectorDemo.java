58b74e5adaeed2459a56f0a2076015e9474e0027
Luis Francisco Contreras Gonzalez <51728784+LuisContrerasGlz@users.noreply.github.com>
2021-10-08 12:47:29 -0500

    Update VectorDemo.java

--- src/CollectionsDemo/VectorDemo.java
+++ src/CollectionsDemo/VectorDemo.java
@@ -5,7 +5,7 @@ import java.util.Vector;
 public class VectorDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Vector<Integer> vs=new Vector<Integer>(2);
 		vs.add(1);
 		vs.add(2);
