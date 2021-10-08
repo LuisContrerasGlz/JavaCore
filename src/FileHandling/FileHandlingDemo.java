3da1b79fcdfa2f57500402c2736e283da8f62ca7
Luis Francisco Contreras Gonzalez <51728784+LuisContrerasGlz@users.noreply.github.com>
2021-10-08 12:44:19 -0500

    Update FileHandlingDemo.java

--- src/FileHandling/FileHandlingDemo.java
+++ src/FileHandling/FileHandlingDemo.java
@@ -6,13 +6,13 @@ public class FileHandlingDemo {
 
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		File file=new File("D://Mexico.txt");
 		try {
 			file.createNewFile();
 			file.delete();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		}
 				
